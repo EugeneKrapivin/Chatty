d05a13961ee6770f3037e97f44ebcfa24ce7cf4d
Eugene Krapivin <evgene.kr@gigya-inc.com>
2016-12-06 16:57:45 +0200

    Initial

---
+++ src/Chatty.Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Runtime;
+using Orleans.Runtime.Configuration;
+
+namespace Chatty.Client
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var config = ClientConfiguration.LocalhostSilo();
+            try
+            {
+                InitializeWithRetries(config, initializeAttemptsBeforeFailing: 5);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Orleans client initialization failed failed due to {ex}");
+
+                Console.ReadLine();
+                return 1;
+            }
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+            return 0;
+        }
+
+        private static void InitializeWithRetries(ClientConfiguration config, int initializeAttemptsBeforeFailing)
+        {
+            var attempt = 0;
+            while (true)
+            {
+                try
+                {
+                    GrainClient.Initialize(config);
+                    Console.WriteLine("Client successfully connect to silo host");
+                    break;
+                }
+                catch (SiloUnavailableException)
+                {
+                    attempt++;
+                    Console.WriteLine($"Attempt {attempt} of {initializeAttemptsBeforeFailing} failed to initialize the Orleans client.");
+                    if (attempt > initializeAttemptsBeforeFailing)
+                    {
+                        throw;
+                    }
+                    Thread.Sleep(TimeSpan.FromSeconds(2));
+                }
+            }
+        }
+    }
+}
---
+++ src/Chatty.Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Orleans.Runtime.Configuration;
+
+namespace Chatty.Server
+{
+    public class Program
+    {
+        private static OrleansHostWrapper _hostWrapper;
+
+        public static int Main(string[] args)
+        {
+            var exitCode = StartSilo(args);
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+
+            exitCode += ShutdownSilo();
+
+            //either StartSilo or ShutdownSilo failed would result on a non-zero exit code. 
+            return exitCode;
+        }
+
+        private static int StartSilo(string[] args)
+        {
+            // define the cluster configuration
+            var config = ClusterConfiguration.LocalhostPrimarySilo();
+            config.AddMemoryStorageProvider();
+
+            // config.Defaults.DefaultTraceLevel = Orleans.Runtime.Severity.Verbose3;
+
+            _hostWrapper = new OrleansHostWrapper(config, args);
+            return _hostWrapper.Run();
+        }
+
+        private static int ShutdownSilo()
+        {
+            return _hostWrapper?.Stop() ?? 0;
+        }
+    }
+}
